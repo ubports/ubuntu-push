0599867d9b66a980af54733c67af656822199443
Samuele Pedroni (Canonical Services Ltd.) <samuele.pedroni@canonical.com>
2014-01-23 15:33:00 +0000

    generalize acceptance.sh

--- server/acceptance/acceptance.sh
+++ server/acceptance/acceptance.sh
@@ -1,6 +1,8 @@
 # run acceptance tests, expects properly setup GOPATH and deps
 # can set extra build params like -race with BUILD_FLAGS envvar
+# can set server pkg name with SERVER_PKG
 set -ex
-go test $BUILD_FLAGS -i
-go build $BUILD_FLAGS -o testserver launchpad.net/ubuntu-push/server/dev
-go test $BUILD_FLAGS -server ./testserver $*
+SERVER_PKG=${SERVER_PKG:-launchpad.net/ubuntu-push/server/dev}
+go test $BUILD_FLAGS -i launchpad.net/ubuntu-push/server/acceptance
+go build $BUILD_FLAGS -o testserver ${SERVER_PKG}
+go test $BUILD_FLAGS launchpad.net/ubuntu-push/server/acceptance -server ${PWD}/testserver $*
