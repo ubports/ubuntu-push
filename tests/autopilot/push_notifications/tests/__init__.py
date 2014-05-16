e6d75d6f1ee75c75483a4e96956518c6909455ec
Richard Huddie <richard.huddie@canonical.com>
2014-05-16 16:45:07 +0100

    clear notifications before running test and move stuff into base class

--- tests/autopilot/push_notifications/tests/__init__.py
+++ tests/autopilot/push_notifications/tests/__init__.py
@@ -15,12 +15,16 @@ from unity8.shell.tests import UnityTestCase
 import unity8.process_helpers as unity8_helpers
 from push_notifications import config as push_config
 import push_notifications.helpers.push_notifications_helper as push_helper
+from testtools.matchers import Equals
+from autopilot.introspection import dbus
 
 
 class PushNotificationTestBase(UnityTestCase):
     """
     Base class for push notification test cases
     """
+    DEFAULT_DISPLAY_MESSAGE = 'There\'s an updated system image.'
+
     @classmethod
     def setUpClass(cls):
         """
@@ -57,6 +61,8 @@ class PushNotificationTestBase(UnityTestCase):
         self.notification_data = self.push_helper.get_device_info()
         # start unity8
         self.unity = self.launch_unity()
+        # dismiss any outstanding dialog
+        self.dismiss_outstanding_dialog()
 
     def create_notification_data_copy(self):
         """
@@ -98,3 +104,84 @@ class PushNotificationTestBase(UnityTestCase):
         Unlock the greeter to display home screen
         """
         unity8_helpers.unlock_unity(self.unity)
+
+    def validate_response(self, response, expected_status_code=200):
+        """
+        Validate the received response status code against expected code
+        """
+        self.assertThat(response.status, Equals(expected_status_code))
+
+    def validate_notification_displayed(self,
+                                         msg_text=DEFAULT_DISPLAY_MESSAGE):
+        """
+        Validate that the notification is displayed
+        Return the dialog object
+        """
+        dialog = self.main_window.wait_select_single(
+            'Notification', objectName='notification1')
+        self.assertEqual(msg_text, dialog.summary)
+        return dialog
+
+    def validate_notification_not_displayed(self, wait=True):
+        """
+        Validate that the notification is not displayed
+        If wait is True then wait for default timeout period
+        If wait is False then do not wait at all
+        """
+        found = True
+        try:
+            if wait is True:
+                self.main_window.wait_select_single(
+                    'Notification', objectName='notification1')
+            else:
+                self.main_window.select_single(
+                    'Notification', objectName='notification1')
+        except dbus.StateNotFoundError:
+            found = False
+        self.assertFalse(found)
+
+    def send_valid_push_message(self):
+        """
+        Send a valid push message which should trigger a notification
+        to be displayed on the client
+        """
+        # create a copy of the device's build info
+        msg_data = self.create_notification_data_copy()
+        # increment the build number to trigger an update
+        msg_data.inc_build_number()
+        # create message based on the data
+        msg = self.push_helper.create_push_message(data=msg_data.json())
+        # send the notification message to the server and check response
+        response = self.push_helper.send_push_broadcast_notification(
+            msg.json(), self.test_config.server_listener_addr)
+        self.validate_response(response)
+
+    def validate_and_dismiss_notification_dialog(self, message):
+        """
+        Validate a notification dialog is displayed and dismiss it
+        """
+        # validate dialog
+        dialog = self.validate_notification_displayed(message)
+        # press dialog to dismiss
+        self.press_notification_dialog(dialog)
+        # check the dialog is no longer displayed
+        self.validate_notification_not_displayed(wait=False)
+
+    def press_notification_dialog(self, dialog):
+        """
+        Press the dialog to dismiss it
+        """
+        self.touch.tap_object(dialog)
+
+    def dismiss_outstanding_dialog(self):
+        """
+        Dismiss outstanding notification dialog that may be displayed
+        from an aborted previous test
+        """
+        try:
+            dialog = self.main_window.select_single(
+                'Notification', objectName='notification1')
+        except dbus.StateNotFoundError:
+            dialog = None
+        if dialog is not None:
+            self.press_notification_dialog(dialog)
--- tests/autopilot/push_notifications/tests/test_push_client.py
+++ tests/autopilot/push_notifications/tests/test_push_client.py
@@ -7,84 +7,14 @@
 
 """Tests for Push Notifications client"""
 
-from testtools.matchers import Equals
-from push_notifications.tests import PushNotificationTestBase
-from autopilot.introspection import dbus
 import time
+from push_notifications.tests import PushNotificationTestBase
 
 
 class TestPushClient(PushNotificationTestBase):
-    """ Tests a Push notification can be sent and received """
-
-    DEFAULT_DISPLAY_MESSAGE = 'There\'s an updated system image.'
-
-    def _validate_response(self, response, expected_status_code=200):
-        """
-        Validate the received response status code against expected code
-        """
-        self.assertThat(response.status, Equals(expected_status_code))
-
-    def _validate_notification_displayed(self,
-                                         msg_text=DEFAULT_DISPLAY_MESSAGE):
-        """
-        Validate that the notification is displayed
-        Return the dialog object
-        """
-        dialog = self.main_window.wait_select_single(
-            'Notification', objectName='notification1')
-        self.assertEqual(msg_text, dialog.summary)
-        return dialog
-
-    def _validate_notification_not_displayed(self, wait=True):
-        """
-        Validate that the notification is not displayed
-        If wait is True then wait for default timeout period
-        If wait is False then do not wait at all
-        """
-        found = True
-        try:
-            if wait is True:
-                self.main_window.wait_select_single(
-                    'Notification', objectName='notification1')
-            else:
-                self.main_window.select_single(
-                    'Notification', objectName='notification1')
-        except dbus.StateNotFoundError:
-            found = False
-        self.assertFalse(found)
-
-    def _send_valid_push_message(self):
-        """
-        Send a valid push message which should trigger a notification
-        to be displayed on the client
-        """
-        # create a copy of the device's build info
-        msg_data = self.create_notification_data_copy()
-        # increment the build number to trigger an update
-        msg_data.inc_build_number()
-        # create message based on the data
-        msg = self.push_helper.create_push_message(data=msg_data.json())
-        # send the notification message to the server and check response
-        response = self.push_helper.send_push_broadcast_notification(
-            msg.json(), self.test_config.server_listener_addr)
-        self._validate_response(response)
-
-    def _validate_and_dismiss_notification_dialog(self, message):
-        """
-        Validate a notification dialog is displayed and dismiss it
-        """
-        # validate dialog
-        dialog = self._validate_notification_displayed(message)
-        # press dialog to dismiss
-        self._press_notification_dialog(dialog)
-        # check the dialog is no longer displayed
-        self._validate_notification_not_displayed(wait=False)
-
-    def _press_notification_dialog(self, dialog):
-        """
-        Press the dialog to dismiss it
-        """
-        self.touch.tap_object(dialog)
+    """
+    Test cases for push notifications
+    """
 
     def test_broadcast_push_notification_screen_off(self):
         """
@@ -94,13 +24,14 @@ class TestPushClient(PushNotificationTestBase):
         # Turn display off
         self.press_power_button()
         # send message
-        self._send_valid_push_message()
+        self.send_valid_push_message()
         # wait before turning screen on
-        time.sleep(5)
+        time.sleep(2)
         # Turn display on
         self.press_power_button()
-
-        self._validate_and_dismiss_notification_dialog(
+        # wait to make sure dialog is displayed
+        time.sleep(2)
+        self.validate_and_dismiss_notification_dialog(
             self.DEFAULT_DISPLAY_MESSAGE)
 
     def test_broadcast_push_notification_locked_greeter(self):
@@ -110,8 +41,8 @@ class TestPushClient(PushNotificationTestBase):
         whist the greeter screen is displayed
         """
         # Assumes greeter starts in locked state
-        self._send_valid_push_message()
-        self._validate_and_dismiss_notification_dialog(
+        self.send_valid_push_message()
+        self.validate_and_dismiss_notification_dialog(
             self.DEFAULT_DISPLAY_MESSAGE)
 
     def test_broadcast_push_notification(self):
@@ -122,8 +53,8 @@ class TestPushClient(PushNotificationTestBase):
         # Assumes greeter starts in locked state
         self.unlock_greeter()
         # send message
-        self._send_valid_push_message()
-        self._validate_and_dismiss_notification_dialog(
+        self.send_valid_push_message()
+        self.validate_and_dismiss_notification_dialog(
             self.DEFAULT_DISPLAY_MESSAGE)
 
     def test_expired_broadcast_push_notification(self):
@@ -140,9 +71,9 @@ class TestPushClient(PushNotificationTestBase):
             msg.json(),
             self.test_config.server_listener_addr)
         # 400 status is received for an expired message
-        self._validate_response(response, expected_status_code=400)
+        self.validate_response(response, expected_status_code=400)
         # validate no notification is displayed
-        self._validate_notification_not_displayed()
+        self.validate_notification_not_displayed()
 
     def test_older_version_broadcast_push_notification(self):
         """
@@ -155,9 +86,9 @@ class TestPushClient(PushNotificationTestBase):
         response = self.push_helper.send_push_broadcast_notification(
             msg.json(),
             self.test_config.server_listener_addr)
-        self._validate_response(response)
+        self.validate_response(response)
         # validate no notification is displayed
-        self._validate_notification_not_displayed()
+        self.validate_notification_not_displayed()
 
     def test_equal_version_broadcast_push_notification(self):
         """
@@ -169,6 +100,6 @@ class TestPushClient(PushNotificationTestBase):
         response = self.push_helper.send_push_broadcast_notification(
             msg.json(),
             self.test_config.server_listener_addr)
-        self._validate_response(response)
+        self.validate_response(response)
         # validate no notification is displayed
-        self._validate_notification_not_displayed()
+        self.validate_notification_not_displayed()
