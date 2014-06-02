41683ffbd8fc491d91e0c2d31020b1bfee2075a1
John R. Lenton <jlenton@gmail.com>
2014-06-02 11:04:34 +0100

    fixed comment on test

--- bus/testing/testing_endpoint_test.go
+++ bus/testing/testing_endpoint_test.go
@@ -76,8 +76,8 @@ func (s *TestingEndpointSuite) TestCallArgs(c *C) {
 		[]callArgs{{"what", []interface{}{"is", "this", "thing"}}})
 }
 
-// Test that Call() fails but does not explode when asked to return
-// values that can't be packed into a dbus message.
+// Test that Call() fails but does not explode when asked to return values
+// that can't be packed into a dbus message.
 func (s *TestingEndpointSuite) TestCallFailsOnBadRetval(c *C) {
 	endp := NewTestingEndpoint(nil, condition.Work(true), Equals)
 	var r uint32
@@ -85,8 +85,8 @@ func (s *TestingEndpointSuite) TestCallFailsOnBadRetval(c *C) {
 	c.Check(e, NotNil)
 }
 
-// Test that Call() fails but does not explode when given argument
-// that can't be packed into a dbus message.
+// Test that Call() fails but does not explode when given an improper result
+// destination (one into which the dbus response can't be stuffed).
 func (s *TestingEndpointSuite) TestCallFailsOnBadArg(c *C) {
 	endp := NewTestingEndpoint(nil, condition.Work(true), 1)
 	r := func() {}
