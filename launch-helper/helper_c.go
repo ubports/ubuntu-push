549e0386dfd0b3f8496d07c92910f7d4f34a7f76
Roberto Alsina <roberto.alsina@canonical.com>
2014-06-18 11:35:01 -0300

    one more test, use table tests

--- launch-helper/helper_c.go
+++ launch-helper/helper_c.go
@@ -13,30 +13,31 @@ void stop_observer (const gchar * appid, const gchar * instanceid, const gchar *
 
 */
 import "C"
-import "fmt"
 import "time"
 
 // These functions are used by tests, they can't be defined in the test file
 // because cgo is not allowed there
 
-func FakeStartLongLivedHelper(helper_type *C.gchar, appid *C.gchar, uris **C.gchar) C.gboolean {
+func fakeStartLongLivedHelper(helper_type *C.gchar, appid *C.gchar, uris **C.gchar) C.gboolean {
 	go func() {
-		fmt.Printf("timeout1\n")
-		time.Sleep(_timelimit * 1000)
-		fmt.Printf("timeout2 calling observer\n")
+		time.Sleep(_timelimit * 3)
 		go_observer()
 	}()
 	return (C.gboolean)(1)
 }
-func FakeStartShortLivedHelper(helper_type *C.gchar, appid *C.gchar, uris **C.gchar) C.gboolean {
+
+func fakeStartShortLivedHelper(helper_type *C.gchar, appid *C.gchar, uris **C.gchar) C.gboolean {
 	go func() {
 		go_observer()
 	}()
 	return (C.gboolean)(1)
 }
 
-func FakeStop(helper_type *C.gchar, appid *C.gchar) C.gboolean {
-	fmt.Printf("Using the fake stop\n")
+func fakeStartFailure(helper_type *C.gchar, appid *C.gchar, uris **C.gchar) C.gboolean {
+	return (C.gboolean)(0)
+}
+
+func fakeStop(helper_type *C.gchar, appid *C.gchar) C.gboolean {
 	return (C.gboolean)(1)
 }
 
--- launch-helper/helper_test.go
+++ launch-helper/helper_test.go
@@ -2,20 +2,25 @@ package main
 
 import "testing"
 
-func TestLongRunningHelper(t *testing.T) {
-	StartHelper = FakeStartLongLivedHelper
-	StopHelper = FakeStop
-	command :=[]string{"foo1", "bar1", "bat1", "baz1"}
-	if runner(command) != helper_stopped {
-		t.Fatalf("Long running helper is not stopped")
-	}
+var runnerTests = []struct {
+	expected int // expected result
+	msg string // description of failure
+	starter func(*_Ctype_gchar, *_Ctype_gchar, **_Ctype_gchar) _Ctype_gboolean // starter fake
+	stopper func(*_Ctype_gchar, *_Ctype_gchar) _Ctype_gboolean // stopper fake
+} {
+	{helper_stopped, "Long running helper is not stopped", fakeStartLongLivedHelper, fakeStop},
+	{helper_finished, "Short running helper doesn't finish", fakeStartShortLivedHelper, fakeStop},
+	{helper_failed, "Filure to start helper doesn't fail", fakeStartFailure, fakeStop},
 }
 
-func TestShortRunningHelper(t *testing.T) {
-	StartHelper = FakeStartShortLivedHelper
-	StopHelper = FakeStop
-	command :=[]string{"foo1", "bar1", "bat1", "baz1"}
-	if runner(command) != helper_finished {
-		t.Fatalf("Short running helper doesn't finish")
+
+func TestRunner(t *testing.T) {
+	for _, tt := range runnerTests {
+		StartHelper = tt.starter
+		StopHelper = tt.stopper
+		command :=[]string{"foo1", "bar1", "bat1", "baz1"}
+		if runner(command) != tt.expected {
+			t.Fatalf(tt.msg)
+		}
 	}
 }
