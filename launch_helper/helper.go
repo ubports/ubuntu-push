ef74d35135edaafafe2e6e3e39e81ff11ef9e911
John R. Lenton <jlenton@gmail.com>
2014-06-30 13:19:14 +0100

    nuked helper-launcher, for now

--- launch_helper/helper.go
+++ launch_helper/helper.go
@@ -18,280 +18,34 @@
 // helpers. The useful part is HelperRunner
 package launch_helper
 
-/*
-#cgo pkg-config: ubuntu-app-launch-2
-#include <stdlib.h>
-#include <ubuntu-app-launch.h>
-#include <glib.h>
-
-void stop_observer(const gchar * appid, const gchar * instanceid, const gchar * helpertype, gpointer user_data);
-*/
-import "C"
-
 import (
-	"errors"
-	"io/ioutil"
-	"os"
-	"path"
-	"strings"
-	"time"
-	"unsafe"
-
-	"launchpad.net/go-xdg/v0"
+	"encoding/json"
 
 	"launchpad.net/ubuntu-push/logger"
 )
 
-type ReturnValue int
-
-const (
-	// HelperStopped means the helper was stopped forcefully
-	HelperStopped ReturnValue = iota
-	// HelperFinished means the helper ended normally
-	HelperFinished
-	// HelperFailed means the helper failed to start
-	HelperFailed
-	// StopFailed means tried to stop the helper but failed
-	StopFailed
-)
-
-const timeLimit = 500 * time.Millisecond
-
-// These are needed for testing because C functions can't be passed
-// around as values
-func _start_helper(helper_type *C.gchar, appid *C.gchar, uris **C.gchar) *C.gchar {
-	return C.ubuntu_app_launch_start_multiple_helper(helper_type, appid, uris)
+type HelperLauncher interface {
+	Run(appId string, message []byte) *HelperOutput
 }
 
-var startHelper = _start_helper
-
-func _stop_helper(helper_type *C.gchar, app_id *C.gchar, instance_id *C.gchar) C.gboolean {
-	return C.ubuntu_app_launch_stop_multiple_helper(helper_type, app_id, instance_id)
+type trivialHelperLauncher struct {
+	log logger.Logger
 }
 
-var stopHelper = _stop_helper
-
-// this channel is global because it needs to be accessed from goObserver which needs
-// to be global to be exported
-var finishedCh = make(chan string, 10)
-
-//export goObserver
-func goObserver(instance_id *C.gchar) {
-	finishedCh <- gostring(instance_id)
+// a trivial HelperLauncher that doesn't launch anything at all
+func NewTrivialHelperLauncher(log logger.Logger) HelperLauncher {
+	return &trivialHelperLauncher{log}
 }
 
-// Convert two strings into a proper NULL-terminated gchar**
-func twoStringsForC(f1 string, f2 string) []*C.gchar {
-	// 3 because we need a NULL terminator
-	ptr := make([]*C.gchar, 3)
-	ptr[0] = gchar(f1)
-	ptr[1] = gchar(f2)
-	return ptr
-}
-
-// run is a wrapper for ubuntu_app_launch_start_multiple_helper
-//
-// XXX: also return an error
-func run(helperType string, appId string, uri1 string, uri2 string) string {
-	helper_type := gchar(helperType)
-	defer free(helper_type)
-	app_id := gchar(appId)
-	defer free(app_id)
-	c_uris := twoStringsForC(uri1, uri2)
-	defer free(c_uris[0])
-	defer free(c_uris[1])
-	instance_id := startHelper(helper_type, app_id, (**C.gchar)(unsafe.Pointer(&c_uris[0])))
-	if instance_id == nil {
-		return ""
+func (triv *trivialHelperLauncher) Run(appId string, message []byte) *HelperOutput {
+	out := new(HelperOutput)
+	err := json.Unmarshal(message, out)
+	if err == nil {
+		return out
 	}
-	defer free(instance_id)
-	return gostring(instance_id)
-}
+	triv.log.Debugf("failed to parse HelperOutput from message, leaving it alone")
+	out.Message = message
+	out.Notification = nil
 
-// stop is a wrapper for ubuntu_app_launch_stop_multiple_helper
-func stop(helperType string, appId string, instanceId string) bool {
-	helper_type := gchar(helperType)
-	defer free(helper_type)
-	app_id := gchar(appId)
-	defer free(app_id)
-	instance_id := gchar(instanceId)
-	defer free(instance_id)
-	success := stopHelper(helper_type, app_id, instance_id)
-	return (C.int)(success) != 0
-}
-
-// HelperArgs represent the arguments for the helper
-type HelperArgs struct {
-	AppId   string
-	Payload []byte
-	Input   string
-	Output  string
-}
-
-// RunnerResult represent the result of running a helper
-type RunnerResult struct {
-	Status ReturnValue
-	Helper *HelperArgs
-	Data   []byte
-	Error  error
-}
-
-// New Creates a HelperRunner
-//
-// log is a logger to use.
-func New(log logger.Logger, helperType string) HelperRunner {
-	input := make(chan *HelperArgs)
-	output := make(chan *RunnerResult)
-	return HelperRunner{
-		log,
-		input,
-		output,
-		helperType,
-	}
-
-}
-
-// Start launches the helper processes received in the helpers channel and
-// puts results in the results channel.
-// Should be called as a goroutine.
-func (hr *HelperRunner) Start() {
-	helper_type := gchar(hr.helperType)
-	defer free(helper_type)
-	// Create an observer to be notified when helpers stop
-	C.ubuntu_app_launch_observer_add_helper_stop(
-		(C.UbuntuAppLaunchHelperObserver)(C.stop_observer),
-		helper_type,
-		nil,
-	)
-	for helper := range hr.HelperCh {
-		// create in/output files
-		pkgName := strings.Split(helper.AppId, "_")[0]
-		err := hr.createTempFiles(helper, pkgName)
-		if err != nil {
-			hr.log.Errorf("failed to create temp files: %v", err)
-			hr.ResultCh <- &RunnerResult{Status: HelperFailed, Helper: helper, Error: err}
-			continue
-		}
-		err = ioutil.WriteFile(helper.Input, helper.Payload, os.ModeTemporary)
-		if err != nil {
-			hr.log.Errorf("failed to write to input file: %v", err)
-			os.Remove(helper.Input)
-			os.Remove(helper.Output)
-			hr.ResultCh <- &RunnerResult{Status: HelperFailed, Helper: helper, Error: err}
-			continue
-		}
-		result := hr.Run(helper.AppId, helper.Input, helper.Output)
-		// read the output file and build the result
-		if result != HelperFinished && result != HelperStopped && result != StopFailed {
-			hr.log.Errorf("helper run failed with: %v, %v", helper, result)
-			os.Remove(helper.Input)
-			os.Remove(helper.Output)
-			hr.ResultCh <- &RunnerResult{Status: result, Helper: helper, Error: errors.New("Helper failed.")}
-			continue
-		}
-		data, err := ioutil.ReadFile(helper.Output)
-		os.Remove(helper.Input)
-		os.Remove(helper.Output)
-		if err != nil {
-			hr.log.Errorf("failed to read output file: %v", err)
-			hr.ResultCh <- &RunnerResult{Status: HelperFailed, Helper: helper, Error: err}
-		} else {
-			hr.ResultCh <- &RunnerResult{Status: result, Helper: helper, Data: data}
-		}
-	}
-}
-
-// HelperRunner is the struct used to launch helpers.
-//
-// HelperCh is the input channel and gets (helperType, appid, file1, file2)
-//
-// ResultCh is the output channel, returns a RunnerResult struct.
-//
-// In that struct, helper is what was used as input and status is one of the ReturnValue constants defined in this package.
-type HelperRunner struct {
-	log        logger.Logger
-	HelperCh   chan *HelperArgs
-	ResultCh   chan *RunnerResult
-	helperType string
-}
-
-// Run starts a helper via ubuntu_app_launch_start_helper, and either
-// waits for it to finish or stops it if more than timeLimit
-// has passed.
-//
-// The return value is a ReturnValue const defined in this package.
-//
-// You probably don't want to run this directly, but instead
-// use Start
-func (hr *HelperRunner) Run(appId string, input string, output string) ReturnValue {
-	hr.log.Debugf("starting helper: %s %s %s %s", hr.helperType, appId, input, output)
-	instanceId := run(hr.helperType, appId, input, output)
-	if instanceId == "" {
-		hr.log.Debugf("failed to start helper")
-		return HelperFailed
-	}
-
-	timeout := time.After(timeLimit)
-	for {
-		select {
-		case <-timeout:
-			hr.log.Debugf("timeout reached, stopping")
-			if stop(hr.helperType, appId, instanceId) {
-				// wait for the stop to come in
-				return HelperStopped
-			} else {
-				return StopFailed
-			}
-		case finishedId := <-finishedCh:
-			if finishedId == instanceId {
-				hr.log.Debugf("finished before timeout, doing nothing")
-				return HelperFinished
-			}
-			hr.log.Debugf("got unexpected instance id %v, ignoring", finishedId)
-		}
-	}
-}
-
-var xdgCacheHome = xdg.Cache.Home
-
-func _getTempDir(pkgName string) (string, error) {
-	tmpDir := path.Join(xdgCacheHome(), pkgName)
-	err := os.MkdirAll(path.Dir(tmpDir), 0700)
-	return tmpDir, err
-}
-
-var getTempDir = _getTempDir
-
-func _getTempFilename(pkgName string) (string, error) {
-	tmpDir, err := getTempDir(pkgName)
-	if err != nil {
-		return "", err
-	}
-	file, err := ioutil.TempFile(tmpDir, "push-helper")
-	defer file.Close()
-	if err != nil {
-		return "", err
-	}
-	return file.Name(), nil
-}
-
-var getTempFilename = _getTempFilename
-
-func (hr *HelperRunner) createTempFiles(helper *HelperArgs, pkgName string) error {
-	var err error
-	if helper.Input == "" {
-		helper.Input, err = getTempFilename(pkgName)
-		if err != nil {
-			hr.log.Errorf("failed to create input file: %v", err)
-			return err
-		}
-	}
-	if helper.Output == "" {
-		helper.Output, err = getTempFilename(pkgName)
-		if err != nil {
-			hr.log.Errorf("failed to create output file: %v", err)
-			return err
-		}
-	}
-	return nil
+	return out
 }
--- launch_helper/helper_test.go
+++ launch_helper/helper_test.go
@@ -17,13 +17,8 @@
 package launch_helper
 
 import (
-	"errors"
-	"io/ioutil"
-	"os"
-	"path"
 	"testing"
 
-	"launchpad.net/go-xdg/v0"
 	. "launchpad.net/gocheck"
 
 	helpers "launchpad.net/ubuntu-push/testing"
@@ -41,236 +36,22 @@ func (s *runnerSuite) SetUpTest(c *C) {
 	s.testlog = helpers.NewTestLogger(c, "error")
 }
 
-var runnerTests = []struct {
-	expected ReturnValue                                                       // expected result
-	msg      string                                                            // description of failure
-	starter  func(*_Ctype_gchar, *_Ctype_gchar, **_Ctype_gchar) *_Ctype_gchar  // starter fake
-	stopper  func(*_Ctype_gchar, *_Ctype_gchar, *_Ctype_gchar) _Ctype_gboolean // stopper fake
-}{
-	{HelperStopped, "Long running helper is not stopped", fakeStartLongLivedHelper, fakeStop},
-	{HelperFinished, "Short running helper doesn't finish", fakeStartShortLivedHelper, fakeStop},
-	{HelperFailed, "Failure to start helper doesn't fail", fakeStartFailure, fakeStop},
-	{HelperFailed, "Error in start argument casting", fakeStartCheckCasting, fakeStop},
-	{StopFailed, "Error in stop argument casting", fakeStartKnownIdDoesNotRun, fakeStopCheckCasting},
+func (s *runnerSuite) TestTrivialRunnerWorks(c *C) {
+	notif := &Notification{Sound: "42"}
+
+	triv := NewTrivialHelperLauncher(s.testlog)
+	// []byte is sent as a base64-encoded string
+	out := triv.Run("foo", []byte(`{"message": "aGVsbG8=", "notification": {"sound": "42"}}`))
+	c.Assert(out, NotNil)
+	c.Check(out.Message, DeepEquals, []byte("hello"))
+	c.Check(out.Notification, DeepEquals, notif)
 }
 
-func (s *runnerSuite) TestARunner(c *C) {
-	for _, tt := range runnerTests {
-		startHelper = tt.starter
-		stopHelper = tt.stopper
-		runner := New(s.testlog, "foo1")
-		result := runner.Run("bar1", "bat1", "baz1")
-		c.Check(result, Equals, tt.expected, Commentf(tt.msg))
-	}
-}
-
-func (s *runnerSuite) TestFileHandling(c *C) {
-	startHelper = fakeStartShortLivedHelper
-	stopHelper = fakeStop
-	hr := New(s.testlog, "test_helper")
-	tmpDir := c.MkDir()
-	inputPath := tmpDir + "/test_helper_input"
-	outputPath := tmpDir + "/test_helper_output"
-	// start the loop inside a function, with a channel to signal when it's done.
-	finished := make(chan bool)
-	go func() {
-		hr.Start()
-		finished <- true
-	}()
-	msg := []byte("{\"msg\": \"foo\"}")
-	ioutil.WriteFile(inputPath, []byte(""), os.ModePerm)
-	ioutil.WriteFile(outputPath, msg, os.ModePerm)
-	helperArgs := &HelperArgs{"bar1", msg, inputPath, outputPath}
-	hr.HelperCh <- helperArgs
-	result := <-hr.ResultCh
-	// check the result
-	expected := &RunnerResult{HelperFinished, helperArgs, msg, nil}
-	c.Check(result, DeepEquals, expected)
-	close(hr.HelperCh)
-	<-finished
-	files, err := ioutil.ReadDir(tmpDir)
-	c.Check(err, IsNil)
-	c.Check(files, HasLen, 0)
-}
-
-func (s *runnerSuite) TestFileHandlingLongRunningHelperOK(c *C) {
-	startHelper = fakeStartLongLivedHelper
-	stopHelper = fakeStop
-	hr := New(s.testlog, "test_helper")
-	tmpDir := c.MkDir()
-	inputPath := tmpDir + "/test_helper_input"
-	outputPath := tmpDir + "/test_helper_output"
-	// start the loop inside a function, with a channel to signal when it's done.
-	finished := make(chan bool)
-	go func() {
-		hr.Start()
-		finished <- true
-	}()
-	msg := []byte("{\"msg\": \"foo\"}")
-	ioutil.WriteFile(inputPath, []byte(""), os.ModePerm)
-	ioutil.WriteFile(outputPath, msg, os.ModePerm)
-	helperArgs := &HelperArgs{"bar1", msg, inputPath, outputPath}
-	hr.HelperCh <- helperArgs
-	result := <-hr.ResultCh
-	// check the result
-	expected := &RunnerResult{HelperStopped, helperArgs, msg, nil}
-	c.Check(result, DeepEquals, expected)
-	close(hr.HelperCh)
-	<-finished
-	files, err := ioutil.ReadDir(tmpDir)
-	c.Check(err, IsNil)
-	c.Check(files, HasLen, 0)
-}
-
-func (s *runnerSuite) TestFileHandlingLongRunningHelperNoOutput(c *C) {
-	startHelper = fakeStartLongLivedHelper
-	stopHelper = fakeStop
-	hr := New(s.testlog, "test_helper")
-	tmpDir := c.MkDir()
-	inputPath := tmpDir + "/test_helper_input"
-	outputPath := tmpDir + "/test_helper_output"
-	// start the loop inside a function, with a channel to signal when it's done.
-	finished := make(chan bool)
-	go func() {
-		hr.Start()
-		finished <- true
-	}()
-	msg := []byte("{\"msg\": \"foo\"}")
-	ioutil.WriteFile(inputPath, []byte(""), os.ModePerm)
-	helperArgs := &HelperArgs{"bar1", msg, inputPath, outputPath}
-	hr.HelperCh <- helperArgs
-	result := <-hr.ResultCh
-	// check the result
-	expected := &RunnerResult{HelperFailed, helperArgs, msg, nil}
-	c.Check(result.Status, Equals, expected.Status)
-	c.Check(result.Helper, DeepEquals, expected.Helper)
-	c.Check(string(result.Data), Equals, "")
-	c.Check(result.Error, ErrorMatches, ".*no such file.*")
-	close(hr.HelperCh)
-	<-finished
-	files, err := ioutil.ReadDir(tmpDir)
-	c.Check(err, IsNil)
-	c.Check(files, HasLen, 0)
-}
-
-func (s *runnerSuite) TestFileHandlingFailed(c *C) {
-	startHelper = fakeStartFailure
-	stopHelper = fakeStop
-	hr := New(s.testlog, "test_helper")
-	tmpDir := c.MkDir()
-	inputPath := tmpDir + "/test_helper_input"
-	outputPath := tmpDir + "/test_helper_output"
-	// start the loop inside a function, with a channel to signal when it's done.
-	finished := make(chan bool)
-	go func() {
-		hr.Start()
-		finished <- true
-	}()
-	msg := []byte("{\"msg\": \"foo\"}")
-	ioutil.WriteFile(inputPath, []byte(""), os.ModePerm)
-	helperArgs := &HelperArgs{"bar1", msg, inputPath, outputPath}
-	hr.HelperCh <- helperArgs
-	result := <-hr.ResultCh
-	// check the result
-	expected := &RunnerResult{HelperFailed, helperArgs, nil, errors.New("Helper failed.")}
-	c.Check(result, DeepEquals, expected)
-	close(hr.HelperCh)
-	<-finished
-	files, err := ioutil.ReadDir(tmpDir)
-	c.Check(err, IsNil)
-	c.Check(files, HasLen, 0)
-}
-
-func (s *runnerSuite) TestFailtoCreateFile(c *C) {
-	startHelper = fakeStartFailure
-	stopHelper = fakeStop
-	// restore it when we are done
-	getTempFilename = func(pkgName string) (string, error) {
-		return "", errors.New("Can't create files.")
-	}
-	defer func() {
-		getTempFilename = _getTempFilename
-	}()
-	hr := New(s.testlog, "test_helper")
-	// start the loop inside a function, with a channel to signal when it's done.
-	finished := make(chan bool)
-	go func() {
-		hr.Start()
-		finished <- true
-	}()
-	helperArgs := &HelperArgs{AppId: "bar1", Payload: []byte(`{"msg": "foo"}`)}
-	hr.HelperCh <- helperArgs
-	result := <-hr.ResultCh
-	// check the result
-	expected := &RunnerResult{HelperFailed, helperArgs, nil, errors.New("Can't create files.")}
-	c.Check(result, DeepEquals, expected)
-	close(hr.HelperCh)
-	<-finished
-}
-
-func (s *runnerSuite) TestCreateTempFiles(c *C) {
-	tmpDir := c.MkDir()
-	getTempDir = func(pkgName string) (string, error) {
-		return tmpDir, nil
-	}
-	// restore it when we are done
-	defer func() {
-		getTempDir = _getTempDir
-	}()
-	helperArgs := &HelperArgs{AppId: "bar1", Payload: []byte{}}
-	c.Check(helperArgs.Input, Equals, "")
-	c.Check(helperArgs.Output, Equals, "")
-	hr := New(s.testlog, "test_helper")
-	err := hr.createTempFiles(helperArgs, helperArgs.AppId)
-	c.Check(err, IsNil)
-	c.Check(helperArgs.Input, Not(Equals), "")
-	c.Check(helperArgs.Output, Not(Equals), "")
-	files, err := ioutil.ReadDir(path.Dir(helperArgs.Input))
-	c.Check(err, IsNil)
-	c.Check(files, HasLen, 2)
-}
-
-func (s *runnerSuite) TestCreateTempFilesWithFilenames(c *C) {
-	tmpDir := c.MkDir()
-	inputPath := tmpDir + "/test_helper_input"
-	outputPath := tmpDir + "/test_helper_output"
-	helperArgs := &HelperArgs{"bar1", []byte{}, inputPath, outputPath}
-	c.Check(helperArgs.Input, Equals, inputPath)
-	c.Check(helperArgs.Output, Equals, outputPath)
-	hr := New(s.testlog, "test_helper")
-	err := hr.createTempFiles(helperArgs, "pkg.name")
-	c.Check(err, IsNil)
-	files, err := ioutil.ReadDir(tmpDir)
-	c.Check(err, IsNil)
-	c.Check(files, HasLen, 0)
-}
-
-func (s *runnerSuite) TestGetTempFilename(c *C) {
-	getTempDir = func(pkgName string) (string, error) {
-		return c.MkDir(), nil
-	}
-	// restore it when we are done
-	defer func() {
-		getTempDir = _getTempDir
-	}()
-	fname, err := getTempFilename("pkg.name")
-	c.Check(err, IsNil)
-	dirname := path.Dir(fname)
-	files, err := ioutil.ReadDir(dirname)
-	c.Check(err, IsNil)
-	c.Check(files, HasLen, 1)
-}
-
-func (s *runnerSuite) TestGetTempDir(c *C) {
-	tmpDir := c.MkDir()
-	xdgCacheHome = func() string {
-		return tmpDir
-	}
-	// restore it when we are done
-	defer func() {
-		xdgCacheHome = xdg.Cache.Home
-	}()
-	dname, err := getTempDir("pkg.name")
-	c.Check(err, IsNil)
-	c.Check(dname, Equals, path.Join(tmpDir, "pkg.name"))
+func (s *runnerSuite) TestTrivialRunnerWorksOnBadInput(c *C) {
+	triv := NewTrivialHelperLauncher(s.testlog)
+	msg := []byte(`this is a not your grandmother's json message`)
+	out := triv.Run("foo", msg)
+	c.Assert(out, NotNil)
+	c.Check(out.Notification, IsNil)
+	c.Check(out.Message, DeepEquals, msg)
 }
