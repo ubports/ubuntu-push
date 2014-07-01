d23a337f6707e6f90071e995191283bdce29db9f
John R. Lenton <jlenton@gmail.com>
2014-07-01 15:49:04 +0100

    dh-golang sucks

--- messaging/cmessaging/cmessaging_c.go
+++ messaging/cmessaging/cmessaging_c.go
@@ -1,18 +1,6 @@
+package cmessaging
+
 /*
- Copyright 2014 Canonical Ltd.
-
- This program is free software: you can redistribute it and/or modify it
- under the terms of the GNU General Public License version 3, as published
- by the Free Software Foundation.
-
- This program is distributed in the hope that it will be useful, but
- WITHOUT ANY WARRANTY; without even the implied warranties of
- MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
- PURPOSE.  See the GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along
- with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #include <glib.h>
 #include <messaging-menu/messaging-menu-app.h>
 #include <messaging-menu/messaging-menu-message.h>
@@ -38,3 +26,5 @@ void add_notification(const gchar* app_id, const gchar* notification_id,
     g_signal_connect(msg, "activate", G_CALLBACK(activate_cb), obj);
 }
 
+*/
+import "C"
