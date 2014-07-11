8b8ede28571971c9aaac4e54ecac97ef73dd22f0
Samuele Pedroni (Canonical Services Ltd.) <samuele.pedroni@canonical.com>
2014-07-11 20:59:10 +0000

    backout spurious bit got through pipelines

--- server/acceptance/cmd/acceptanceclient.go
+++ server/acceptance/cmd/acceptanceclient.go
@@ -43,7 +43,7 @@ type configuration struct {
 	// server connection config
 	Addr        config.ConfigHostPort
 	CertPEMFile string   `json:"cert_pem_file"`
-	AuthHelper  string   `json:"auth_helper"`
+	AuthHelper  []string `json:"auth_helper"`
 }
 
 func main() {
@@ -90,7 +90,9 @@ func main() {
 		}
 	}
 	if len(cfg.AuthHelper) != 0 {
-		auth, err := exec.Command(cfg.AuthHelper, "https://push.ubuntu.com/").Output()
+		helperArgs := cfg.AuthHelper[1:]
+		helperArgs = append(helperArgs, "https://push.ubuntu.com/")
+		auth, err := exec.Command(cfg.AuthHelper[0], helperArgs...).Output()
 		if err != nil {
 			log.Fatalf("auth helper: %v", err)
 		}
